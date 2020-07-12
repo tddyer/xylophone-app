f9c3977060d09a1c09a6d9a57cdcaf63a2896795
Tanner <tdyer@hawk.iit.edu>
2020-07-12 14:01:19 -0500

    implemented basic audio playing from a local file when a button is pressed using the audioplayers dart package

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  static AudioCache player = new AudioCache();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Text(nouns.first),
+          child: Center(
+            child: RaisedButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Play Audio'),
+            ),
+          ),
         ),
       ),
     );
